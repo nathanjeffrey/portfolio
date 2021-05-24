6584d1d2fa0b1dffd69a36300249ff1ef5f11b75
Nathan Yung <jeff@Nathans-MacBook-Air.local>
2021-05-25 00:38:24 +0100

    changed grid to flex boxes

--- pages/index.js
+++ pages/index.js
@@ -46,35 +46,39 @@ export default function Home() {
           </p>
         </div>
         <div className={styles.grid}>
-          <div className={styles.customCard}>
-            <h2 className={styles.centerAlign}>Front-end Development</h2>
-            <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+          <div className={styles.row}>
+            <div className={styles.customCard}>
+              <h2 className={styles.centerAlign}>ReactJS and React Native</h2>
+              <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+            </div>
+            <div className={styles.customCard}>
+              <h2 className={styles.centerAlign}>Front-end Development</h2>
+              <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+            </div>
           </div>
 
-          <div className={styles.customCard}>
-            <h2 className={styles.centerAlign}>ReactJS and React Native</h2>
-            <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
-          </div>
-
-          <div className={styles.customCard}>
-            <h2 className={styles.centerAlign}>Next.JS</h2>
-            <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
-          </div>
-
-          <div className={styles.customCard}>
-            <h2 className={styles.centerAlign}>HTML {'&'} CSS</h2>
-            <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+          <div className={styles.row}>
+            <div className={styles.customCard}>
+              <h2 className={styles.centerAlign}>Next.JS</h2>
+              <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+            </div>
+            <div className={styles.customCard}>
+              <h2 className={styles.centerAlign}>HTML {'&'} CSS</h2>
+              <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+            </div>
           </div>
           
-          <div className={styles.customCard}>
-            <h2 className={styles.centerAlign}>Multilinguality</h2>
-            <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+          <div className={styles.row}>
+            <div className={styles.customCard}>
+              <h2 className={styles.centerAlign}>Multilinguality</h2>
+              <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+            </div>
+            <div className={styles.customCard}>
+              <h2 className={styles.centerAlign}>Proficiency in Communication</h2>
+              <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
+            </div>
           </div>
 
-          <div className={styles.customCard}>
-            <h2 className={styles.centerAlign}>Proficiency in Communication</h2>
-            <p className={styles.centerAlign}>Find in-depth information about Next.js features and API.</p>
-          </div>
         </div>
       </main>
 
